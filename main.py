5af623920a105703d808dfad73129a7f72ed5daf
hsuanyu414 <93172849+hsuanyu414@users.noreply.github.com>
2024-01-02 21:19:17 +0800

    feat: slider added
    1. y, z 方向視圖
    2. x, y, z, bar

--- main.py
+++ main.py
@@ -31,9 +31,18 @@ class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ct_image = extend.astype(np.uint8)
 
         # default index, should be determined by the slider
-        self.x_index = 256
-        self.y_index = 256
-        self.z_index = 330
+        self.x_index = 0
+        self.y_index = 0
+        self.z_index = 0
+
+        # define thickness of line
+        self.thickness = 2
+        self.rect_length = 50
+
+        # link slider 
+        self.x_slice_slider.valueChanged.connect(self.x_slice_slider_changed)
+        self.y_slice_slider.valueChanged.connect(self.y_slice_slider_changed)
+        self.z_slice_slider.valueChanged.connect(self.z_slice_slider_changed)
 
     def qimg2np(self, qimg):
         # input qimg is a QImage object and output arr is a numpy array
@@ -69,23 +78,72 @@ class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         image_temp = self.ct_image[self.x_index, :, ::-1].T
         image_temp = cv2.cvtColor(image_temp, cv2.COLOR_GRAY2RGB)
         # draw line on image according to y_index and z_index
-        thickness = 2 # TODO: should be a global variable
-        image_temp[self.y_index-thickness:self.y_index+thickness, :, 1] = 255
-        image_temp[:, self.z_index-thickness:self.z_index+thickness, 1] = 255
-        image_temp_focus = image_temp[self.y_index-50:self.y_index+50, self.z_index-50:self.z_index+50, :].copy()
-        rect_length = 50 # TODO: should be a global variable
-        cv2.rectangle(image_temp, (self.z_index-rect_length, self.y_index-rect_length), (self.z_index+rect_length, self.y_index+rect_length), (0, 255, 0), thickness)
+        image_temp[:, self.y_index-self.thickness:self.y_index+self.thickness, 1] = 255
+        image_temp[self.z_index-self.thickness:self.z_index+self.thickness, :, 1] = 255
+        image_temp_focus = image_temp[self.z_index-50:self.z_index+50, self.y_index-50:self.y_index+50, :].copy()
+        cv2.rectangle(image_temp, (self.y_index-self.rect_length, self.z_index-self.rect_length), (self.y_index+self.rect_length, self.z_index+self.rect_length), (0, 255, 0), self.thickness)
         self.show_image(image_temp, self.x_view)
         self.show_image(image_temp_focus, self.x_view_focus)
+
+
+    def show_y_image(self):
+        # show y image main view and focus view
+        image_temp = self.ct_image[:, self.y_index, ::-1].T
+        image_temp = cv2.cvtColor(image_temp, cv2.COLOR_GRAY2RGB)
+        # draw line on image according to x_index and z_index
+        image_temp[:, self.x_index-self.thickness:self.x_index+self.thickness, 1] = 255
+        image_temp[self.z_index-self.thickness:self.z_index+self.thickness, :, 1] = 255
+        image_temp_focus = image_temp[self.z_index-50:self.z_index+50, self.x_index-50:self.x_index+50, :].copy()
+        cv2.rectangle(image_temp, (self.x_index-self.rect_length, self.z_index-self.rect_length), (self.x_index+self.rect_length, self.z_index+self.rect_length), (0, 255, 0), self.thickness)
+        self.show_image(image_temp, self.y_view)
+        self.show_image(image_temp_focus, self.y_view_focus)
+
+    def show_z_image(self):
+        # show z image main view and focus view
+        image_temp = self.ct_image[:, :, self.z_index].T
+        image_temp = cv2.cvtColor(image_temp, cv2.COLOR_GRAY2RGB)
+        # draw line on image according to x_index and y_index
+        image_temp[:, self.x_index-self.thickness:self.x_index+self.thickness, 1] = 255
+        image_temp[self.y_index-self.thickness:self.y_index+self.thickness, :, 1] = 255
+        image_temp_focus = image_temp[self.y_index-50:self.y_index+50, self.x_index-50:self.x_index+50, :].copy()
+        cv2.rectangle(image_temp, (self.x_index-self.rect_length, self.y_index-self.rect_length), (self.x_index+self.rect_length, self.y_index+self.rect_length), (0, 255, 0), self.thickness)
+        self.show_image(image_temp, self.z_view)
+        self.show_image(image_temp_focus, self.z_view_focus)
+
 
     def show_ct_image(self):
         # show ct image in each graphicsView
         self.show_x_image() # use this function to show x image (and y, z image also)
-
+        self.show_y_image() 
+        self.show_z_image()
         # self.show_image(self.ct_image[self.x_index, :, ::-1].T, self.x_view)
         # self.show_image(self.ct_image[:, self.y_index, ::-1].T, self.y_view)
         # self.show_image(self.ct_image[:, :, self.z_index].T, self.z_view)
-        
+
+    def slider_range(self):
+        # set slider range according to ct image
+        self.x_slice_slider.setMinimum(0)
+        self.x_slice_slider.setMaximum(self.ct_image.shape[0]-1)
+        self.y_slice_slider.setMinimum(0)
+        self.y_slice_slider.setMaximum(self.ct_image.shape[1]-1)
+        self.z_slice_slider.setMinimum(0)
+        self.z_slice_slider.setMaximum(self.ct_image.shape[2]-1)
+
+    def x_slice_slider_changed(self):
+        # change x_index according to slider
+        self.x_index = self.x_slice_slider.value()
+        self.show_ct_image()    
+
+    def y_slice_slider_changed(self):
+        # change y_index according to slider
+        self.y_index = self.y_slice_slider.value()
+        self.show_ct_image()
+
+    def z_slice_slider_changed(self):
+        # change z_index according to slider
+        self.z_index = self.z_slice_slider.value()
+        self.show_ct_image()
+
                 
 
 if __name__ == '__main__':
@@ -93,6 +151,7 @@ if __name__ == '__main__':
     window = MyMainWindow()
     window.show()
     
+    window.slider_range()
     window.show_ct_image() 
     # do this for test, should be called in read_image function implemented
     
